d3fb140ffd8b2cc4a7de428cbcd72eb8b58bd323
MINHO CHOI <minhocalgary@gmail.com>
2020-06-16 15:57:47 -0600

    add gitigonre

--- credential/AWS.js
+++
@@ -1,8 +0,0 @@
-
-exports.access = {
-    accessKeyId: "AKIA5DWR6HFBO4UKBPE3",
-    secretAccessKey: "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",
-}
-
-
-    
--- credential/mongoDb.js
+++
@@ -1,5 +0,0 @@
-
-exports.mongoDB = 
-    "mongodb+srv://admin:password2020@cluster0-xuivl.mongodb.net/Cars?retryWrites=true&w=majority";
-
-
